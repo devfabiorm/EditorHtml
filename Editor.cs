036f679167266085510a9b20aa7a115784257d65
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-05-25 17:53:16 -0300

    Implemeted method to allow user to wrtite and save files

---
+++ Editor.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("--------------------");
+
+            Start();
+        }
+
+        public static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Deseja salvar o aquivo? [S]im ou [N]ão");
+
+            var option = Console.ReadLine();
+            var choice = ConvertChoice(option);
+
+            if (choice)
+            {
+                Console.WriteLine("Forneça o caminho para salvar o arquivo");
+                HandleOptionSelected(Console.ReadLine(), file.ToString());
+            }
+        }
+
+        public static void HandleOptionSelected(string path, string content)
+        {
+            using var file = new StreamWriter(path);
+            file.Write(content);
+        }
+
+        public static bool ConvertChoice(string option)
+        {
+            return option.StartsWith("s", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
--- Menu.cs
+++ Menu.cs
@@ -7,5 +7,70 @@ static class Menu
         Console.Clear();
         Console.BackgroundColor = ConsoleColor.Blue;
         Console.ForegroundColor = ConsoleColor.Black;
+        DrawScreen();
+        WriteOptions();
+
+        var option = short.Parse(Console.ReadLine());
+
+        HandleMenuOption(option);
+    }
+
+    public static void DrawScreen()
+    {
+        DrawLines('+', '-', '+', true);
+        for (int lines = 0; lines <= 10; lines++)
+        {
+            DrawLines('|', ' ', '|', true);
+        }
+        DrawLines('+', '-', '+', false);
+    }
+
+    public static void DrawLines(char startChar, char separatorChar, char endChar, bool jumpLine)
+    {
+        Console.Write(startChar);
+        for (int i = 0; i <= 30; i++)
+            Console.Write(separatorChar);
+        Console.Write(endChar);
+
+        if (jumpLine)
+            Console.Write("\n");
+    }
+
+    public static void WriteOptions()
+    {
+        WriteLine(3, 2, "Editor HTML");
+        WriteLine(3, 3, "===================");
+        WriteLine(3, 4, "Selecione uma opção abaixo");
+        WriteLine(3, 6, "1 - Novo Arquivo");
+        WriteLine(3, 7, "2 - Abrir");
+        WriteLine(3, 9, "0 - Sair");
+        WriteLine(3, 10, "Opção: ", false);
+
+    }
+
+    public static void WriteLine(int col, int line, string text, bool jumpLine = true)
+    {
+        Console.SetCursorPosition(col, line);
+
+        if (jumpLine)
+            Console.WriteLine(text);
+        else
+            Console.Write(text);
+    }
+
+    public static void HandleMenuOption(short option)
+    {
+        switch (option)
+        {
+            case 1: Editor.Show(); break;
+            case 2: Console.WriteLine("View"); break;
+            case 0:
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+            default: Show(); break;
+        }
     }
 }
